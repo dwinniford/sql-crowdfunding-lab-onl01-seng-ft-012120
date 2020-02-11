b86b1f40f10ff4f55b0548ad48bafd7c045d3e50
Dave Winniford <dwinniford@gmail.com>
2020-02-11 17:00:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,3 +20,13 @@ INSERT INTO users (name, age) VALUES ("Bob", 21);
 INSERT INTO users (name, age) VALUES ("Bob", 21);
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ( "Hot-pot camel back", "kitchen utensil", 230, 02202020, 03012020);
