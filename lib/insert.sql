c5206e7fc939ab2cfa79837c99a0730450fae509
Dave Winniford <dwinniford@gmail.com>
2020-02-11 16:56:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,3 +18,5 @@ INSERT INTO users (name, age) VALUES ("Bob", 21);
 INSERT INTO users (name, age) VALUES ("Bob", 21);
 INSERT INTO users (name, age) VALUES ("Bob", 21);
 INSERT INTO users (name, age) VALUES ("Bob", 21);
+
+INSERT INTO projects (name)
